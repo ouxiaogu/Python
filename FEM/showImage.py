3d7abe96eee8db69e88d0fc5c7c1085bf92c116c
Peng-cheng Yang <peyang@asml.com>
2017-03-09 11:31:08 +0800

    Thursday, March 09, 2017 11:12:39 AM, ignore some folder

--- FEM/showImage.py
+++
@@ -1,9 +0,0 @@
-import os, os.path
-import matplotlib.pyplot as plt
-import sysimp
-
-imagepath=sys.argv[1]
-#imfile = os.path.join(workpath, 'Lenna.png')
-img = plt.imread(imagepath)
-plt.imshow(img)
-plt.show()
