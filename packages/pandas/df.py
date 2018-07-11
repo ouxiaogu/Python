561cb077a1bf5b1c1ed7bb89af08a12628cd9e71
ouxiaogu <oypengcheng@gmail.com>
2018-07-11 10:22:09 +0800

    force sync

--- packages/pandas/df.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-d = {'one' : pd.Series([1., 2., 3.], index=['a', 'b', 'c']),
-    'two' : pd.Series([1., 2., 3., 4.], index=['a', 'b', 'c', 'd'])}
-df = pd.DataFrame(d)
-print(d)
-print(df)
-
